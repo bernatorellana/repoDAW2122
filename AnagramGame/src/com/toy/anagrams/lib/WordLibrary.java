20c216bbd882dde4208a40841dd57bc54e426887
bernat <bernat.orellana@gmail.com>
2022-01-27 17:38:27 +0100

    Branca main: canvi 2

--- AnagramGame/src/com/toy/anagrams/lib/WordLibrary.java
+++ AnagramGame/src/com/toy/anagrams/lib/WordLibrary.java
@@ -76,4 +76,8 @@ public abstract class WordLibrary {
      * @return true if the guess was correct; false otherwise
      */
     public abstract boolean isCorrect(int idx, String userGuess);
+	
+	
+	public void saluda(){
+	}
 }
